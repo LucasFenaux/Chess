929f6ff74adcbb131c45a45b99b84743866ce13b
Lucas Fenaux <46611744+LucasFenaux@users.noreply.github.com>
2019-08-20 22:31:17 -0400

    center window on screen and size dependent on display

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.locals import *
 import sys
+import os
+import ctypes
 from Piece import Piece
 from Board import Board
 from Pawn import Pawn
@@ -9,21 +11,28 @@ from Bishop import Bishop
 from Knight import Knight
 
 
-def main():
+def set_up_game():
     pygame.init()
+    os.environ['SDL_VIDEO_CENTERED'] = '1'  # centers the window
     black = 0, 0, 0
-    background_screen_size = (1024, 1024)
-    background_screen = pygame.display.set_mode(background_screen_size, HWSURFACE | DOUBLEBUF | RESIZABLE)
+    user32 = ctypes.windll.user32
+    screensize = (int(user32.GetSystemMetrics(0) - user32.GetSystemMetrics(0) / 8),
+                  int(user32.GetSystemMetrics(1) - user32.GetSystemMetrics(1) / 8))  # leaves some breathing room
+    screen_size = (1024, 1024)
+    background_screen_size = screensize
+    background_screen = pygame.display.set_mode(screen_size, HWSURFACE | DOUBLEBUF | RESIZABLE)
     background_screen.fill(black)
     screen = background_screen.copy()
-    background_screen.blit(screen, (0, 0))
+    background_screen = pygame.display.set_mode(background_screen_size, HWSURFACE | DOUBLEBUF | RESIZABLE)
+    background_screen.blit(pygame.transform.scale(screen, background_screen_size), (0, 0))
     background = Board(screen)
     rook = Rook([0, 0], 'white', background)
     rook.move([1, 0])
     i = 0
     while 1:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.QUIT:
+                sys.exit()
             elif event.type == VIDEORESIZE:
                 new_size = event.dict['size']
                 background_screen = pygame.display.set_mode(new_size, HWSURFACE | DOUBLEBUF | RESIZABLE)
@@ -39,4 +48,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    set_up_game()

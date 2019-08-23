2dbeef7418ab2247f7737e05b95fd73fa6359c7d
Lucas Fenaux <46611744+LucasFenaux@users.noreply.github.com>
2019-08-23 17:25:42 -0400

    fixed and added clarity to the click handling
    you can now unselect a piece by clicking on it after selecting it

--- src/RegularGame.py
+++ src/RegularGame.py
@@ -11,6 +11,8 @@ class RegularGame:
         self.background_screen = background_screen
         self.board = Board(screen)
         self.first_selected_square = None
+        self.turn = 'white'
+        self.state = 0
         self.scale = (screen.get_size()[0] / self.background_screen.get_size()[0],
                       screen.get_size()[1] / self.background_screen.get_size()[1])
 
@@ -26,7 +28,7 @@ class RegularGame:
         else:
             piece_factory.populate_regular_game_bb()
 
-    def handle_click(self, first_click):
+    def handle_click(self, is_down):
         pos = pygame.mouse.get_pos()
         scaled_pos = (int(pos[0] * self.scale[0]), int(pos[1] * self.scale[1]))
         selected_square = None
@@ -34,13 +36,39 @@ class RegularGame:
             if square.rect.collidepoint(scaled_pos):
                 selected_square = square
                 break
-        no_sq_or_no_piece = self.first_selected_square is None or self.first_selected_square.get_piece() is None
-        if no_sq_or_no_piece and first_click:
-            self.first_selected_square = selected_square
-        elif not no_sq_or_no_piece and not first_click:
-            moved = self.first_selected_square.get_piece().move(selected_square)
-            if not moved:
-                print("invalid move, failed to move")
+        if self.state == 0 and is_down:
+            if selected_square.get_piece() is not None and selected_square.get_piece().get_color() == self.turn:
+                self.first_selected_square = selected_square
+                self.state = 1
+        elif self.state == 1 and not is_down:
+            if selected_square != self.first_selected_square:
+                moved = self.first_selected_square.get_piece().move(selected_square)
+                if not moved:
+                    print("invalid move, failed to move")
+                else:
+                    if self.turn == 'white':
+                        self.turn = 'black'
+                    else:
+                        self.turn = 'white'
+                self.state = 0
+            else:
+                self.state = 2
+        elif self.state == 2 and is_down:
+            self.state = 3
+        elif self.state == 3 and not is_down:
+            if self.first_selected_square == selected_square:
+                self.first_selected_square = None
+                self.state = 0
+            else:
+                moved = self.first_selected_square.get_piece().move(selected_square)
+                if not moved:
+                    print("invalid move, failed to move")
+                else:
+                    if self.turn == 'white':
+                        self.turn = 'black'
+                    else:
+                        self.turn = 'white'
+                self.state = 0
 
     def update_background_size(self, new_size):
         self.background_screen = pygame.display.set_mode(new_size, HWSURFACE | DOUBLEBUF | RESIZABLE)
